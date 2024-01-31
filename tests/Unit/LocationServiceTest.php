da9f75de991e932a4d9e44ffc6a900cb479f0a1f
Michael <mike98989@gmail.com>
2024-01-31 12:40:35 +0000

    Worked on Creation of a new location on Location Service

---
+++ tests/Unit/LocationServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Location;
+use Illuminate\Http\Request;
+use App\Service\LocationService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LocationServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     */
+    public function test_if_location_is_created(): void
+    {
+        $departmentService = new LocationService();
+        $data_array = ['location'=>'location1','state'=>'state1','zone'=>'zone1'];
+        $data = new Request($data_array);
+        $result = $departmentService->create($data);
+        $this->assertInstanceOf(Location::class, $result);
+        $this->assertNotNull($result->id);
+        $this->assertDatabaseHas('locations', [
+            'location'=>'location1','state'=>'state1','zone'=>'zone1'
+        ]);        
+    }
+}
