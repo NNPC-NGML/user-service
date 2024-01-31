96f0513fefb647e471db749d85948be9d17d5fac
michael <mike98989@gmail.com>
2024-01-31 15:26:08 +0100

    WIP

--- tests/Unit/LocationServiceTest.php
+++ tests/Unit/LocationServiceTest.php
@@ -27,4 +27,16 @@ class LocationServiceTest extends TestCase
             'location'=>'location1','state'=>'state1','zone'=>'zone1'
         ]);        
     }
+
+    public function test_if_location_is_not_created(): void
+    {
+        $locationService = new LocationService();
+        $data_array = ['location'=>'new location'];
+        $data = new Request($data_array);
+        $createDepartment = $locationService->create($data);
+        $resultArray = $createDepartment->toArray();
+        $this->assertNotEmpty($createDepartment);
+        $this->assertIsArray($resultArray);
+        $this->assertArrayHasKey('state', $resultArray);
+    }
 }
