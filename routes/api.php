e664440cd80be33d11e4ec9f5c007ef875bcf3f8
Brisstone <okolijohnson69@gmail.com>
2024-02-19 15:40:30 +0100

    OJ_service-test

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,5 @@ Route::group(['prefix' => 'v1'], function () {
 
     //Location
     Route::delete('/locations/{id}', [LocationController::class, 'delete'])->name('locations.delete');
+    Route::get('/locations', [LocationController::class, 'index'])->name('locations.index');
 });
