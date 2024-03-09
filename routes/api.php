d1f972346f0f5449d5e6318407024de29f0981d6
Brisstone <okolijohnson69@gmail.com>
2024-03-09 23:48:21 +0100

    OJ-controller-tests

--- routes/api.php
+++ routes/api.php
@@ -48,5 +48,5 @@ Route::group(['prefix' => 'v1'], function () {
 
     //Unit
     Route::post('/create_unit',[UnitController::class, 'create'])->name('create_unit');
-
+    Route::put('/update_unit/{id}',[UnitController::class, 'update'])->name('units.update');
 });
--- tests/Feature/UnitControllerTest.php
+++ tests/Feature/UnitControllerTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\department;
+use App\Models\Unit;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UnitControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test */
     public function it_can_create_a_unit(): void
     {
@@ -46,6 +49,87 @@ class UnitControllerTest extends TestCase
                 'success' => false,
                 'error' => [
                     'description' => ['The description field is required.'],
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function it_updates_unit_record()
+    {
+        $department = department::factory()->create();
+
+        $unit1 = Unit::create([
+            'name' => 'Unit 2',
+            'description' => 'Description 2',
+            'department_id' => $department->id,
+        ]);
+
+        $updatedUnit = [
+            'name' => 'Unit 1',
+            'description' => 'Description 1',
+            'department_id' => $department->id,
+        ];
+
+        $response = $this->putJson(route('units.update', ['id' => $unit1->id]), $updatedUnit);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'data' => [
+                    'name' => $updatedUnit['name'],
+                    'description' => $updatedUnit['description'],
+                ]
+            ]);
+
+        $this->assertDatabaseHas('units', [
+            'id' => $unit1->id,
+            'description' => $updatedUnit['description'],
+            'name' => $updatedUnit['name'],
+        ]);
+    }
+
+    /** @test */
+    public function it_returns_unit_record_not_found()
+    {
+
+        $nonExistentUnitId = mt_rand(1000000000, 9999999999);
+
+        $response = $this->putJson(route('units.update', ['id' => $nonExistentUnitId]), []);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'success' => false,
+                'error' => false
+            ]);
+    }
+
+    /** @test */
+    public function it_validates_unit_update_request_data()
+    {
+        $department = department::factory()->create();
+
+        $unit = Unit::create([
+            'name' => 'Unit 2',
+            'description' => 'Description 2',
+            'department_id' => $department->id,
+        ]);
+
+        $updatedUnit = [
+            'name' => '',
+            'description' => '',
+            'department_id' => $department->id,
+        ];
+
+        $response = $this->putJson(route('units.update', ['id' => $unit->id]), $updatedUnit);
+
+        $response->assertStatus(422);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'success' => false,
+                'error' => [
+                    'description' => ['The description field is required.'],
+                    'name' => ['The name field is required.']
                 ]
             ]);
     }
