5293f87ca2d3b195e550cd6cc7414e8b1f29aa68
Brisstone <okolijohnson69@gmail.com>
2024-04-10 17:59:29 +0100

    OJ-merge-resolve

--- routes/api.php
+++ routes/api.php
@@ -48,14 +48,11 @@ Route::group(['prefix' => 'v1'], function () {
 
     //Unit
     Route::post('/create_unit',[UnitController::class, 'create'])->name('create_unit');
-<<<<<<< HEAD
     Route::put('/update_unit/{id}',[UnitController::class, 'update'])->name('units.update');
-=======
     Route::get('/units/{departmentId}',[UnitController::class, 'getUnitsInDepartment'])->name('show_units_in_department');
     Route::get('/units', [UnitController::class, 'index'])->name('units.index');
     Route::delete('/unit/{id}',[UnitController::class, 'destroy'])->name('delete_unit');
     Route::get('/unit/{id}', [UnitController::class, 'show'])->name('units.show');
 
 
->>>>>>> 8a66ef941a64032e30329274fb5e15ec98b4f3be
 });
