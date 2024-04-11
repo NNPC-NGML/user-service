5f83fd23594a9d057589a061779759c7151aac6c
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-11 13:38:29 +0100

    wip

--- tests/Feature/Queue/LocationQueueTest.php
+++ tests/Feature/Queue/LocationQueueTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature\Queue;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Location;
+use Illuminate\Support\Facades\Queue;
+use App\Jobs\Location\LocationCreated;
+use App\Jobs\Location\LocationDeleted;
+use App\Jobs\Location\LocationUpdated;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LocationQueueTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_it_dispatches_location_created_job()
+    {
+        Queue::fake();
+        $location = Location::factory()->create();
+        LocationCreated::dispatch($location->toArray());
+        Queue::assertPushed(LocationCreated::class, function ($job) use ($location) {
+            return $job->getData() == $location->toArray();
+        });
+    }
+
+    public function test_it_dispatches_location_updated_job()
+    {
+        Queue::fake();
+        $location = Location::factory()->create();
+
+        LocationUpdated::dispatch($location->toArray());
+        Queue::assertPushed(LocationUpdated::class, function ($job) use ($location) {
+            return $job->getData() == $location->toArray();
+        });
+    }
+
+    public function test_it_dispatches_location_deleted_job(): void
+    {
+        Queue::fake();
+        $location = Location::factory()->create();
+
+        LocationDeleted::dispatch($location->id);
+        Queue::assertPushed(LocationDeleted::class, function ($job) use ($location) {
+            return $job->getId() == $location->id;
+        });
     }
 }
